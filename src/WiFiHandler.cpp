394ac875a88b5d72054f1153bfa5391476d1db54
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2023-05-20 15:44:10 +0200

    Warp DEBUG

--- src/WiFiHandler.cpp
+++ src/WiFiHandler.cpp
@@ -8,8 +8,10 @@
 void WiFiHandler::initWifi() {
     int wifiWaitCount = 0;
     WiFiClass::setHostname(DeviceName); //maybe cstr
+#ifdef DEBUG
     Serial.print("\nWIFI: Connecting to ");
     Serial.println(WIFI_SSID);
+#endif
     WiFi.begin(WIFI_SSID, WIFI_PW);
 
     while (WiFiClass::status() != WL_CONNECTED && wifiWaitCount < 20)
@@ -17,7 +19,7 @@ void WiFiHandler::initWifi() {
         delay(250);
         wifiWaitCount++;
     }
-
+#ifdef DEBUG
     if (WiFiClass::status() == WL_CONNECTED)
     {
         Serial.println("WIFI: connected.");
@@ -28,6 +30,7 @@ void WiFiHandler::initWifi() {
     {
         Serial.println("WIFI: not connected");
     }
+#endif
 }
 bool WiFiHandler::StatusCheck()
 {
@@ -36,21 +39,24 @@ bool WiFiHandler::StatusCheck()
     {
         ReStart();
     }
+#ifdef DEBUG
     else
     {
         Serial.println("WIFI: Still connected");
         Serial.println();
     }
+#endif
     return status == WL_CONNECTED;
 }
 
 void WiFiHandler::ReStart()
 {
+#ifdef DEBUG
     // Connect to WiFi network
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(WIFI_SSID);
-
+#endif
     WiFi.begin(WIFI_SSID, WIFI_PW);
     int wifiWaitCount = 0;
     while (WiFiClass::status() != WL_CONNECTED && wifiWaitCount < 20)
@@ -58,6 +64,7 @@ void WiFiHandler::ReStart()
         delay(250);
         wifiWaitCount++;
     }
+#ifdef DEBUG
     if (WiFiClass::status() == WL_CONNECTED)
     {
         Serial.println();
@@ -66,6 +73,7 @@ void WiFiHandler::ReStart()
         // Print the IP address
         Serial.println(WiFi.localIP());
     }
+#endif
 }
 
 bool WiFiHandler::checkWifi(unsigned long currentSeconds) {
