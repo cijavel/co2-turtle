179438ddbb1be6494ef47f878aa4db1639f80acb
cijavel <Konto--github@l7t.de>
2023-07-03 02:36:19 +0200

    FastLed: Configuration, data transfer, main

--- src/Configuration.h
+++ src/Configuration.h
@@ -2,13 +2,14 @@
 #define CO2_TURTLE_CONFIGURATION_H
 
 //uncomment to get debug in serial
-#define DEBUG 1
+#define DEBUG 0
 
-#define interval_MHZ19_in_Seconds 30
-#define interval_BME680_in_Seconds 30
-#define interval_WiFiCheck_in_Seconds 60
-#define interval_RAMPrintout_in_Seconds 30
-#define interval_EPD_in_Seconds 180
+#define interval_MHZ19_in_Seconds 60
+#define interval_BME680_in_Seconds 60
+#define interval_WiFiCheck_in_Seconds 120
+#define interval_RAMPrintout_in_Seconds 600
+#define interval_EPD_in_Seconds 700
+#define interval_LED_in_Seconds 60
 
 #define PIN_BME680_SDA 21
 #define PIN_BME680_SCL 22
@@ -18,6 +19,12 @@
 #define DeviceName "SensorTurtle 1"
 
 
+#define BRIGHTNESS_LEDS 10
+#define NUM_LEDS 34
+#define DATA_LED_PIN 4
 
+#define MAX_POWER_MILLIAMPS 500
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
 
 #endif //CO2_TURTLE_CONFIGURATION_H
--- src/FastLedHandler.h
+++ src/FastLedHandler.h
@@ -4,7 +4,8 @@
 #include "Configuration.h"
 #include "CO2Data.h"
 #include "bsec.h"
-#define FASTLED_ALL_PINS_HARDWARE_SPI
+
+#include <WiFi.h>
 #include <SPI.h>
 #include <FastLED.h>
 #include <SectionManager.h>
@@ -15,18 +16,18 @@ public:
         static FastLedHandler instance; // Guaranteed to be destroyed.
         return instance;// Instantiated on first use.
     }
-    void fastLedWiFi();
-    void fastLedBME();
-    void fastLedCO2();
-    void fastinit();
+
     void addLEDsection();
     void setup_led();
-
+    bool ledstatus(const unsigned long currentSeconds);
+    void setCo2AndData(CO2Data co2Sensordata, Bsec enviromentdata);
 
 
 private:
-    uint16_t rainbowAllSections(uint8_t pauseDuration, uint16_t wheelPosition, uint16_t multi);
-
+    void ledStatusWiFi();
+    void ledStatusBME();
+    void ledStatusCO2();
+    
     CO2Data co2data;
     Bsec bmedata;
     FastLedHandler() {};                    // Constructor? (the {} brackets) are needed here.
