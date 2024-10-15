667f9ad02faf2edcaecb676ba132531759be3d11
cijavel <Konto--github@l7t.de>
2024-10-16 00:52:52 +0200

    major: add AD mode

--- src/WiFiHandler.cpp
+++ src/WiFiHandler.cpp
@@ -1,25 +1,64 @@
 #include <WiFi.h>
 #include "WiFiHandler.h"
-
 // please rename credentials_example.h to credentials.h and set your WIFI Credentials there
+#include <Preferences.h>
 #include "Credentials.h"
 #include "Configuration.h"
+#include "WebServerHandler.h"
+extern Preferences preferences;
+String password;
+String ssid;
 
-void WiFiHandler::initWifi() {
+void WiFiHandler::loadWiFiCredentials()
+{
+    preferences.begin("wifi", true);
+
+    ssid = preferences.getString("ssid", "");
+    password = preferences.getString("password", "");
+    preferences.end();
+
+    if (ssid == "" || password == "")
+    {
+        ssid = WIFI_SSID;
+        password = WIFI_PW;
+        Serial.println("using default credentials. No stored WiFi credentials found");
+    }
+    else
+    {
+        Serial.println("Stored WiFi credentials loaded.");
+    }
+
+    Serial.print("SSID: ");
+    Serial.println(ssid);
+}
+
+void WiFiHandler::setupAPMode()
+{
+    const char *apSSID = "sensorturle 192.168.4.1";
+    const char *apPassword = "sensorturtle";
+
+    WiFi.softAP(apSSID, apPassword);
+    Serial.println("[AP MODE] Access Point Started");
+    Serial.print("[AP MODE] IP address: ");
+    Serial.println(WiFi.softAPIP());
+}
+
+void WiFiHandler::initWifi()
+{
     int wifiWaitCount = 0;
-    WiFiClass::setHostname(DeviceName); //maybe cstr
+    WiFiClass::setHostname(DeviceName);
 #ifdef DEBUG
     Serial.print("\n[WIFI] Connecting to ");
     Serial.println(WIFI_SSID);
 #endif
-    WiFi.begin(WIFI_SSID, WIFI_PW);
+    loadWiFiCredentials();
+    WiFi.begin(ssid.c_str(), password.c_str());
 
     while (WiFiClass::status() != WL_CONNECTED && wifiWaitCount < 20)
     {
         delay(250);
         wifiWaitCount++;
     }
-#ifdef DEBUG
     if (WiFiClass::status() == WL_CONNECTED)
     {
         Serial.println("[WIFI] connected.");
@@ -28,7 +67,32 @@ void WiFiHandler::initWifi() {
     }
     else
     {
-        Serial.println("[WIFI] not connected");
+        Serial.println("[WIFI] Could not connect. Starting AP mode. Please connect to the wlan, which was created by the esp32");
+        setupAPMode();
+    }
+}
+
+void WiFiHandler::ReStart()
+{
+#ifdef DEBUGk
+    Serial.println();
+    Serial.print("[WIFI] Connecting to ");
+    Serial.println(WIFI_SSID);
+#endif
+    loadWiFiCredentials();
+    WiFi.begin(ssid.c_str(), password.c_str());
+    int wifiWaitCount = 0;
+    while (WiFiClass::status() != WL_CONNECTED && wifiWaitCount < 20)
+    {
+        delay(250);
+        wifiWaitCount++;
+    }
+#ifdef DEBUG
+    if (WiFiClass::status() == WL_CONNECTED)
+    {
+        Serial.println();
+        Serial.println("[WIFI] WiFi connected");
+        Serial.println(WiFi.localIP());
     }
 #endif
 }
@@ -50,37 +114,11 @@ bool WiFiHandler::StatusCheck()
     return status == WL_CONNECTED;
 }
 
-void WiFiHandler::ReStart()
+bool WiFiHandler::checkWifi(unsigned long currentSeconds)
 {
-#ifdef DEBUG
-    // Connect to WiFi network
-    Serial.println();
-    Serial.print("[WIFI] Connecting to ");
-    Serial.println(WIFI_SSID);
-#endif
-    WiFi.begin(WIFI_SSID, WIFI_PW);
-    int wifiWaitCount = 0;
-    while (WiFiClass::status() != WL_CONNECTED && wifiWaitCount < 20)
+    if (currentSeconds % interval_WiFiCheck_in_Seconds == 0)
     {
-        delay(250);
-        wifiWaitCount++;
-    }
-#ifdef DEBUG
-    if (WiFiClass::status() == WL_CONNECTED)
-    {
-        Serial.println();
-        Serial.println("[WIFI] WiFi connected");
-
-        // Print the IP address
-        Serial.println(WiFi.localIP());
-    }
-#endif
-}
-
-bool WiFiHandler::checkWifi(unsigned long currentSeconds) {
-    if (currentSeconds % interval_WiFiCheck_in_Seconds == 0) {
         return WiFiHandler::StatusCheck();
     }
     return false;
 }
-
--- src/WiFiHandler.h
+++ src/WiFiHandler.h
@@ -1,11 +1,15 @@
 #ifndef CO2_TURTLE_WIFIHANDLER_H
 #define CO2_TURTLE_WIFIHANDLER_H
+
 class WiFiHandler{
 public:
     static void initWifi();
     static bool checkWifi(unsigned long currentSeconds);
+    static void setupAPMode();
 private:
     static bool StatusCheck();
     static void ReStart();
+    static void loadWiFiCredentials();
 };
+
 #endif //CO2_TURTLE_WIFIHANDLER_H
