b2cfc2b7c86fee672a2a2cef741896648c43e84d
cijavel <Konto--github@l7t.de>
2025-04-27 22:30:07 +0200

    major: fix settings calls and display on settings webpage

--- src/SettingsHandler.cpp
+++ src/SettingsHandler.cpp
@@ -1,6 +1,30 @@
 #include "settingsHandler.h"
 #include "Configuration.h"
 #include "credentials.h"
+#include <map>
+
+static std::map<String, int> settingMap = {};
+
+void SettingsHandler::loadsavedSettings() {
+    preferences.begin("config", true);
+       settingMap["switchWIFI"] = preferences.getBool("switchWIFI", switch_WIFI);
+       settingMap["switchEPD"] = preferences.getBool("switchEPD", switch_EPD);
+       settingMap["switchLED"] = preferences.getBool("switchLED", switch_LED);
+       settingMap["switchMQTT"] = preferences.getBool("switchMQTT", switch_MQTT);
+
+       settingMap["intervalMHZ19"] = preferences.getInt("intervalMHZ19", interval_MHZ19_in_Seconds);
+       settingMap["intervalBME680"] = preferences.getInt("intervalBME680", interval_BME680_in_Seconds);
+       settingMap["intervalWiFi"] = preferences.getInt("intervalWiFi", interval_WiFiCheck_in_Seconds);
+       settingMap["intervalPRINT"] = preferences.getInt("intervalPRINT", interval_RAMPrintout_in_Seconds);
+       settingMap["intervalEPD"] = preferences.getInt("intervalEPD", interval_EPD_in_Seconds);
+       settingMap["intervalLED"] = preferences.getInt("intervalLED", interval_LED_in_Seconds);
+       settingMap["intervalMQTT"] = preferences.getInt("intervalMQTT", interval_mqtt_in_Seconds);
+    preferences.end();
+}
+
+int SettingsHandler::getSetting(String settingName){
+ return settingMap[settingName];
+}
 
 void SettingsHandler::loadDefaultSettings() {
     Serial.println("[preferences] set configuration");
@@ -43,12 +67,16 @@ void SettingsHandler::reset() {
     preferences.end();
 }
 void SettingsHandler::setSeetingsOnFirstRun() {
-    preferences.begin("config", false); 
-    bool hasRunBefore = preferences.getBool("setSeetingsFirstRun", false); 
+    preferences.begin("config", true); 
+        bool hasRunBefore = preferences.getBool("setSeetingsFirstRun", false); 
+    preferences.end();
     if (!hasRunBefore) {
         loadDefaultSettings();
-        preferences.putBool("setSeetingsFirstRun", true);
+        preferences.begin("config", false); 
+            preferences.putBool("setSeetingsFirstRun", true);
+        preferences.end();
         Serial.println("[Settings] Setting firstRun flag in NVS to true.");
     } 
-    preferences.end();
+    loadsavedSettings();
+    
 }
