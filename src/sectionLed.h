ab6420a4cb81f9930006a3b45c3517f9d77ef12d
cijavel <Konto--github@l7t.de>
2025-05-03 06:16:33 +0200

    minor: format

--- src/sectionLed.h
+++ src/sectionLed.h
@@ -2,19 +2,21 @@
 #define SECTIONLED_H
 
 // Define an enumeration for section names
-enum SectionName {
-    LED_TEMP,
-    LED_HUM,
-    LED_WLANCONNECT,
-    LED_PRES,
-    LED_CO2,
-    NUM_SECTIONS
+enum SectionName
+{
+	LED_TEMP,
+	LED_HUM,
+	LED_WLANCONNECT,
+	LED_PRES,
+	LED_CO2,
+	NUM_SECTIONS
 };
 
 // Define the sections (start and end indices)
-struct SectionStruc {
-    int startLed;
-    int endLed;
+struct SectionStruc
+{
+	int startLed;
+	int endLed;
 };
 
 #endif // SECTIONLED_H
