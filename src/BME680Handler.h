9213451d0f3c557baedb19eab2fed91ac757b444
cijavel <Konto--github@l7t.de>
2026-02-28 12:56:37 +0100

    fix: iaqAccuracy problem may be solve with that

--- src/BME680Handler.h
+++ src/BME680Handler.h
@@ -26,7 +26,6 @@ private:
     BME680Handler();
     BME680Handler(BME680Handler const &);
     void operator=(BME680Handler const &); 
-    void updateSensorDataInternal();
     unsigned long _lastRunSeconds = 0;
 };
 #endif // CO2_TURTLE_BME680HANDLER_H
