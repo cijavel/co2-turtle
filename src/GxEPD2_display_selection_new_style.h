32d07bb2dd96805741ef6ca92926d23bd7f859d9
cijavel <Konto--github@l7t.de>
2026-03-11 15:56:52 +0100

    refactor(epd): clean up GxEPD2_display_selection_new_style.h
    Remove GxEPD2_1248/1248c block (12.48" display, irrelevant),
    LOLIN_D32_PRO branch, and unused IS_GxEPD2_1248 macros.
    Pin config now uses EPD_PIN_RST/BUSY from Configuration.h.
    Reduces file from 90 to 45 lines.

--- src/GxEPD2_display_selection_new_style.h
+++ src/GxEPD2_display_selection_new_style.h
@@ -1,4 +1,3 @@
-#include "Configuration.h"
 // Display Library example for SPI e-paper panels from Dalian Good Display and boards from Waveshare.
 // Requires HW SPI and Adafruit_GFX. Caution: the e-paper panels require 3.3V supply AND data lines!
 //
@@ -17,48 +16,30 @@
 // NOTE: you may need to adapt or select for your wiring in the processor specific conditional compile sections below
 
 // select the display class (only one), matching the kind of display panel
-//#define GxEPD2_DISPLAY_CLASS GxEPD2_BW
+
+#include "Configuration.h"  // for EPD_PIN_RST, EPD_PIN_BUSY
+
 #define GxEPD2_DISPLAY_CLASS GxEPD2_3C
-//#define GxEPD2_DISPLAY_CLASS GxEPD2_7C
+#define GxEPD2_DRIVER_CLASS  GxEPD2_213_Z98c // GDEY0213Z98 122x250, SSD1680
 
-
-// 3-color e-papers
-//#define GxEPD2_DRIVER_CLASS GxEPD2_154c     // GDEW0154Z04 200x200, IL0376F, no longer available
-//#define GxEPD2_DRIVER_CLASS GxEPD2_154_Z90c // GDEH0154Z90 200x200, SSD1681
-//#define GxEPD2_DRIVER_CLASS GxEPD2_213c     // GDEW0213Z16 104x212, UC8151 (IL0373)
-//#define GxEPD2_DRIVER_CLASS GxEPD2_213_Z19c // GDEH0213Z19 104x212, UC8151D
-#define GxEPD2_DRIVER_CLASS GxEPD2_213_Z98c // GDEY0213Z98 122x250, SSD1680
-//#define GxEPD2_DRIVER_CLASS GxEPD2_290c     // GDEW029Z10  128x296, UC8151 (IL0373)
-//#define GxEPD2_DRIVER_CLASS GxEPD2_290_Z13c // GDEH029Z13  128x296, UC8151D
-//#define GxEPD2_DRIVER_CLASS GxEPD2_290_C90c // GDEM029C90  128x296, SSD1680
-
-
-
-// SS is usually used for CS. define here for easy change
 #ifndef EPD_CS
-
-#define EPD_CS SS
+#define EPD_CS SS  // GPIO5
 #endif
 
 #if defined(GxEPD2_DISPLAY_CLASS) && defined(GxEPD2_DRIVER_CLASS)
 
-// somehow there should be an easier way to do this
 #define GxEPD2_BW_IS_GxEPD2_BW true
 #define GxEPD2_3C_IS_GxEPD2_3C true
 #define GxEPD2_7C_IS_GxEPD2_7C true
-#define GxEPD2_1248_IS_GxEPD2_1248 true
-#define GxEPD2_1248c_IS_GxEPD2_1248c true
 #define IS_GxEPD(c, x) (c##x)
 #define IS_GxEPD2_BW(x) IS_GxEPD(GxEPD2_BW_IS_, x)
 #define IS_GxEPD2_3C(x) IS_GxEPD(GxEPD2_3C_IS_, x)
 #define IS_GxEPD2_7C(x) IS_GxEPD(GxEPD2_7C_IS_, x)
-#define IS_GxEPD2_1248(x) IS_GxEPD(GxEPD2_1248_IS_, x)
-#define IS_GxEPD2_1248c(x) IS_GxEPD(GxEPD2_1248c_IS_, x)
 
 #include "GxEPD2_selection_check.h"
 
 #if defined(ESP32)
-#define MAX_DISPLAY_BUFFER_SIZE 65536ul // e.g.
+#define MAX_DISPLAY_BUFFER_SIZE 65536ul
 #if IS_GxEPD2_BW(GxEPD2_DISPLAY_CLASS)
 #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
 #elif IS_GxEPD2_3C(GxEPD2_DISPLAY_CLASS)
@@ -66,24 +47,9 @@
 #elif IS_GxEPD2_7C(GxEPD2_DISPLAY_CLASS)
 #define MAX_HEIGHT(EPD) (EPD::HEIGHT <= (MAX_DISPLAY_BUFFER_SIZE) / (EPD::WIDTH / 2) ? EPD::HEIGHT : (MAX_DISPLAY_BUFFER_SIZE) / (EPD::WIDTH / 2))
 #endif
-// adapt the constructor parameters to your wiring
-#if !IS_GxEPD2_1248(GxEPD2_DRIVER_CLASS) && !IS_GxEPD2_1248c(GxEPD2_DRIVER_CLASS)
-#if defined(ARDUINO_LOLIN_D32_PRO)
-#else
-
 
 GxEPD2_DISPLAY_CLASS<GxEPD2_DRIVER_CLASS, MAX_HEIGHT(GxEPD2_DRIVER_CLASS)> display(GxEPD2_DRIVER_CLASS(/*CS=*/ EPD_PIN_CS, /*DC=*/ EPD_PIN_DC, /*RST=*/ EPD_PIN_RST, /*BUSY=*/ EPD_PIN_BUSY));
 
-
-#endif
-#else // GxEPD2_1248 or GxEPD2_1248c
-// Waveshare 12.48 b/w or b/w/r SPI display board and frame or Good Display 12.48 b/w panel GDEW1248T3 or b/w/r panel GDEY1248Z51
-// general constructor for use with all parameters, e.g. for Waveshare ESP32 driver board mounted on connection board
-GxEPD2_DISPLAY_CLASS < GxEPD2_DRIVER_CLASS, MAX_HEIGHT(GxEPD2_DRIVER_CLASS) > display(GxEPD2_DRIVER_CLASS(/*sck=*/ 13, /*miso=*/ 12, /*mosi=*/ 14,
-	/*cs_m1=*/ 23, /*cs_s1=*/ 22, /*cs_m2=*/ 16, /*cs_s2=*/ 19,
-	/*dc1=*/ 25, /*dc2=*/ 17, /*rst1=*/ 33, /*rst2=*/ 5,
-	/*busy_m1=*/ 32, /*busy_s1=*/ 26, /*busy_m2=*/ 18, /*busy_s2=*/ 4));
-#endif
 #undef MAX_DISPLAY_BUFFER_SIZE
 #undef MAX_HEIGHT
 #endif
